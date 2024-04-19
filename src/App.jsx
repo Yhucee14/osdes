614fd497de1b20d9d8eb5a1adfaa05c75a04d213
Yhucee14 <uchechukwu7814@gmail.com>
2024-04-19 03:44:10 -0700

    finished nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import "./index.css";
 import { useState, useEffect } from "react";
+import logo from "./images/logo.png";
+import { CgMenuGridR } from "react-icons/cg";
+import { AiFillCloseCircle } from "react-icons/ai";
+
 
 const App = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -25,43 +29,49 @@ const App = () => {
 
   return (
     <div className=" flex flex-col text-red ">
-      <nav className=" w-full bg-black text-white text-lg  py-4 px-4 z-10 shadow-md">
+      <nav className=" w-full bg-black text-white text-lg py-4 sm:px-4 xx:px-1 z-10 shadow-md">
         <div>
           {isMobile ? (
             // Mobile view with menu icon
             <div className="flex items-center justify-between flex-row px-3">
-              {/* <CgMenuGridR
-            className='w-7 h-7 text-[#ffd100] cursor-pointer hover:w-9 hover:h-9 absolute top-4 right-7'
-            onClick={() => setIsDropdownOpen(true)}
-          /> */}{" "}
-              <div className="flex items-center text-yellow">
-                <a className="flex items-center justify-center  sm:justify-center ">
-                  {/* <img
-                      src={logo}
-                      alt="logo"
-                      className="w-full h-14 sm:w-full xx:py-1  xx:px-2 sm:p-2 sm:h-12 object-cover object-center "
-                    /> */}{" "}
-                  logo
+              <CgMenuGridR
+                className="w-8 h-8 text-yellow cursor-pointer hover:w-9 hover:h-9 hover:text-red absolute xx:top-6 sm:top-9 right-7"
+                onClick={() => setIsDropdownOpen(true)}
+              />{" "}
+              <div className="flex justify-start sm:w-[200px] xx:w-[150px] ">
+                <a
+                  href="./"
+                  target={"_blank"}
+                  className="flex  text-yellow justify-start sm:justify-center "
+                >
+                  <img
+                    src={logo}
+                    alt="logo"
+                    className=" xx:py-1 xx:px-2 sm:p-2 object-cover object-center "
+                  />{" "}
                 </a>
               </div>
-              <div
+              {/* <div
                 className="cursor-pointer text-yellow absolute top-4 right-7"
                 onClick={() => setIsDropdownOpen(true)}
               >
                 open
-              </div>
+              </div> */}
             </div>
           ) : (
             // Desktop view with navigation links
-            <div className="flex px-2 justify-between">
-              <div className="flex items-center">
-                <a className="flex items-center text-yellow justify-center sm:justify-center ">
-                  {/* <img
-                      src={logo}
-                      alt="logo"
-                      className="w-full h-14 sm:w-full xx:py-1  xx:px-2 sm:p-2 sm:h-12 object-cover object-center "
-                    /> */}{" "}
-                  logo
+            <div className="flex  justify-between">
+              <div className="flex justify-start w-[200px] ">
+                <a
+                  href="./"
+                  target={"_blank"}
+                  className="flex  text-yellow justify-start sm:justify-center "
+                >
+                  <img
+                    src={logo}
+                    alt="logo"
+                    className=" xx:py-1 xx:px-2 sm:p-2 object-cover object-center "
+                  />{" "}
                 </a>
               </div>
 
@@ -105,11 +115,7 @@ const App = () => {
 
               <div className="flex items-center flex-row ">
                 <button className="flex justify-between flex-row font-semibold py-2 px-2 rounded-md shadow-sm bg-yellow text-black border-2 border-white hover:bg-white hover:text-red">
-                  <a
-                    href=""
-                    target={"_blank"}
-                    className="px-1"
-                  >
+                  <a href="" target={"_blank"} className="px-1">
                     Get Ticket
                   </a>
                 </button>
@@ -117,16 +123,19 @@ const App = () => {
             </div>
           )}
 
-      
-
           {isMobile && isDropdownOpen && (
             <div className="text-black py-4">
-              <div
+              {/* <div
                 className="cursor-pointer absolute text-yellow top-4 right-10"
                 onClick={() => setIsDropdownOpen(false)}
               >
                 close
-              </div>
+              </div> */}
+
+              < AiFillCloseCircle
+                className="w-10 h-10 text-yellow cursor-pointer hover:w-9 hover:h-9 absolute xx:top-5 sm:top-8 right-7 "
+                onClick={() => setIsDropdownOpen(false)}
+              />
 
               {/* Add your mobile navigation links here */}
               <div className="flex flex-col justify-between items-center bg-black text-white rounded-md   py-6">
@@ -171,9 +180,7 @@ const App = () => {
         </div>
       </nav>
 
-      <div className="text-black text-2xl p-20 text-center">
-        carousel
-      </div>
+      <div className="text-black text-2xl p-20 text-center">carousel</div>
 
       <div className="text-red bg-white text-2xl p-20 border-b-2  text-center">
         Schedule
@@ -186,7 +193,7 @@ const App = () => {
       <div className="text-red text-2xl bg-white p-20 border-b-2 text-center">
         get involved
       </div>
-      
+
       <div className="text-red text-2xl bg-white p-20 border-b-2 text-center">
         gallery
       </div>
@@ -198,7 +205,6 @@ const App = () => {
       <footer className="p-10 text-2xl bg-black text-yellow text-center">
         footer
       </footer>
-
     </div>
   );
 };
